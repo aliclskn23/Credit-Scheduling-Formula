067f6e0ebcd95cc9af88bcd71eff359012cd2ebd
ali caliskan <caliskanali5534@gmail.com>
2024-02-25 20:14:36 +0300

    credit scheduling formula

---
+++ credi.js
@@ -0,0 +1,35 @@
+function krediHesapla() {
+    var cekilecekTutar, VadeSayisi;
+    var aylikTaksit, OdenecekToplamTutar;
+  
+    cekilecekTutar = document.getElementById("txtKrediTutari").value;
+  
+    var liste = document.getElementById("listeVade");
+    VadeSayisi = liste.options[liste.selectedIndex].value;
+  
+    if (VadeSayisi == 12) {
+      OdenecekToplamTutar = cekilecekTutar * 1.3;
+    } else if (VadeSayisi == 24) {
+      OdenecekToplamTutar = cekilecekTutar * 1.4;
+    } else if (VadeSayisi == 36) {
+      OdenecekToplamTutar = cekilecekTutar * 1.5;
+    } else if (VadeSayisi == 48) {
+      OdenecekToplamTutar = cekilecekTutar * 1.7;
+    }
+    aylikTaksit = OdenecekToplamTutar / VadeSayisi;
+  
+    document.querySelector("#sonuc").innerHTML =
+      "Toplam Geri Ödeme: " +
+      OdenecekToplamTutar.toLocaleString("tr-TR", {
+        style: "currency",
+        currency: "TRY",
+        minimumFractionDigits: 2,
+      }) +
+      " | Aylık Taksit Tutarı: " +
+      aylikTaksit.toLocaleString("tr-TR", {
+        style: "currency",
+        currency: "TRY",
+        minimumFractionDigits: 2,
+      });
+  }
+  
